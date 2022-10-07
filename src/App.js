6d30300e516cbf9f2062848b5f04d961a30f1c65
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2022-10-07 17:41:42 +0530

    + Home page + Reg Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/client_dashboard/ClientDashboard.js'
 
 function App() {
   return (
     <div className="App">
     <div className = "text-2xl text-red-600">
-      hello world!
-    </div>
+      <Home/>
+      </div>
     </div>
   );
 }
